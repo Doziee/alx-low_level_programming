20e2bd5febf1fba036c86ec4bb5159f0f5e12494
Chiedozie <ikechukwuedozie00@gmail.com>
2022-07-05 17:58:35 -0700

    Updating

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+i#include "main.h"
+/**
+ * _islower - check for lowercse character
+ * @c: check input for the character
+ * Return: int.
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+return (1);
+else
+return (0)
+}
