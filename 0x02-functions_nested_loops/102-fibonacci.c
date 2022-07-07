c373271d942761ef77a92f8fe458ee5fbfbd9823
Chiedozie <ikechukwuedozie00@gmail.com>
2022-07-06 19:27:21 -0700

    Done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print the add of Fibonacci
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long i = 0;
+
+	long j = 1;
+
+	int n = 0;
+
+	long fib;
+
+	while (n < 50)
+	{
+		fib = j + i;
+		if (n != 49)
+			printf("%ld, ", fib);
+			else
+				printf("%ld\n", fib);
+			i = j;
+			j = fib;
+			n += 1;
+	}
+		return (0);
+}
